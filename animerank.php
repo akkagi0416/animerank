7bbc176f10a0abb0e8e03d96ef0af51d53feb8eb
akkagi0416 <akkagi0416@gmail.com>
2015-06-22 13:47:54 +0900

    make Animerank class & getFollowersCount

--- animerank.php
+++ animerank.php
@@ -12,11 +12,36 @@ $consumerSecret     = $keytoken['consumerSecret'];
 $accessToken        = $keytoken['accessToken'];
 $accessTokenSecret  = $keytoken['accessTokenSecret'];
 
-$connection = new TwitterOAuth( $consumerKey, $consumerSecret, $accessToken, $accessTokenSecret );
+// $connection = new TwitterOAuth( $consumerKey, $consumerSecret, $accessToken, $accessTokenSecret );
 
-$api = 'users/show';
-$content = $connection->get( $api, array(
-    'screen_name' => 'akkagi0416',
-    ) );
+// $api = 'users/show';
+// $content = $connection->get( $api, array(
+//     'screen_name' => 'akkagi0416',
+// ) );
 
-var_dump( $content );
+// echo $content->{'statuses_count'};  // ツイート数
+// echo $content->{'friends_count'};   // フォロー数
+// echo $content->{'followers_count'}; // フォロワー数
+// var_dump( $content );
+
+class Animerank
+{
+    private $connection;
+
+    function __construct( $consumerKey, $consumerSecret, $accessToken, $accessTokenSecret )
+    {
+        $this->connection = new TwitterOAuth( $consumerKey, $consumerSecret, $accessToken, $accessTokenSecret );
+    }
+    function getFollowersCount( $screen_name )
+    {
+        $api = 'users/show';
+        $userinfo = $this->connection->get( $api, array(
+            'screen_name' => 'akkagi0416',
+        ) );
+        // var_dump( $userinfo );
+        return $userinfo->{'followers_count'}; 
+    }
+}
+
+$a = new Animerank( $consumerKey, $consumerSecret, $accessToken, $accessTokenSecret );
+echo $a->getFollowersCount( 'akkagi0416' );
